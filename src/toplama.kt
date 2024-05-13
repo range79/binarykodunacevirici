29beebecde8e1aeadd1550709f062fb2c6b5e496
range79 <nija4elements@gmail.com>
2024-05-13 03:50:34 +0300

    fonksiyon

--- src/toplama.kt
+++ src/toplama.kt
@@ -1,2 +1,32 @@
-class toplama {
+fun yeniden_baslat(){
+    println("Programı yeniden başlatmak istiyor musunuz? (e/h)")
+    val test: String = readLine() ?: ""
+    when(test.toUpperCase()){
+        "y", "e", "yes", "evet" -> programRestart()
+        else -> {}
+    }
+}
+
+fun programRestart() {
+    println("Kod 3 saniye içinde yeniden başlıyor...")
+    for (i in 3 downTo 0) {
+        Thread.sleep(1000)
+        println(i)
+    }
+    println("\n\n\n\n\n\n\n\n\n\n")
+    return main()
+}
+
+fun yalnis_islem(){
+    println("yalnis tusa basildi")
+    programRestart()
+}
+fun bolme(x:Double,y:Double){
+    if (y==0.00){
+        println("0 a bolemezsiniz")
+        programRestart()
+    }
+    else{
+        println(x/y)
+    }
 }
