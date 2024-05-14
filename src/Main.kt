def341d8e603fbc21ece84a55968d19ac7743337
range79 <nija4elements@gmail.com>
2024-05-14 08:53:42 +0300

    yeni surum

--- src/Main.kt
+++ src/Main.kt
@@ -1,88 +1,69 @@
-
-
-
-fun main(){
-   println("aritmetrik islem icin '1' e  \n10dalik rakami binarye donusturmek icin  '2'ye basiniz:" )
-    val kesir: String = readln()
-    when(kesir)
-    {
-        "2"->{
-            print("donusturmek istedigin rakami gir:")
-
-val x= readln().toInt()
-           println( decimalToBinary(x))
-           yeniden_baslat()
-
-            }
-
-
-       "1"->{
-            println("ilk sayiyi giriniz")
-            val x:Double?= readln().toDoubleOrNull()
-
-            if (x==null){
-                println("gecersiz islem girdiniz")
-              programRestart()
-
-                return main()
-            }
-
-            else{
-
-                println("ikinci sayiyi giriniz")
-                val y: Double? = readln().toDoubleOrNull()
-                if (y==null) {
-                println("deger girmediniz")
-                 programRestart()
-
-                    }
-                else{
-
-                    println("yapmak istediginiz islemi giriniz")
-                    val islem:String = readln()
-
-                    when (islem) {
-                        "*" -> println(x * y)
-                        "/" -> bolme(x, y)
-                        "+" -> println(x + y)
-
-                        "-" -> println(x - y)
-                        "toplaminin karakoku" -> println((x + y) * (x + y))
-                        "mod" -> println(x % y)
-                        else -> {
-
-                          yalnis_islem()
-                        }
-                    }
+import kotlin.math.pow
+fun main() {
+    println("Aritmetik işlem için '1' e\n10'luk rakamı binary'e dönüştürmek için '2'ye\nBinary'den 10'luk'a çevirmek için '3'e\nSözleri binary'e dönüştürmek için '4'e basınız")
+    val secim: String = readln()
+    when (secim) {
+        "4" -> {
+            println("Dönüştüreceğiniz sözü, harfi veya cümleyi giriniz:")
+            val x: String = readln()
+            println(stringToBinary(x))
             yeniden_baslat()
+        }
+        "3" -> {
+            println("Binary rakamını giriniz:")
+            val x: String = readlnOrNull() ?: run {
+                println("Değer girmediniz")
+                yeniden_baslat()
+                return
+            }
+            println(binaryToDecimal(x))
+            yeniden_baslat()
+        }
+        "2" -> {
+            println("Dönüştürmek istediğiniz rakamı giriniz:")
+            val x = readln().toIntOrNull() ?: run {
+                println("Geçersiz değer girdiniz")
+                yeniden_baslat()
+                return
+            }
+            println(decimalToBinary(x))
+            yeniden_baslat()
+        }
+        "1" -> {
+            println("İlk sayıyı giriniz:")
+            val x: Double = readln().toDoubleOrNull() ?: run {
+                println("Geçersiz değer girdiniz")
+                yeniden_baslat()
+                return
+            }
 
+            println("İkinci sayıyı giriniz:")
+            val y: Double = readln().toDoubleOrNull() ?: run {
+                println("Geçersiz değer girdiniz")
+                yeniden_baslat()
+                return
+            }
+
+            println("Yapmak istediğiniz işlemi giriniz (+, -, *, /, toplamının karakökü, mod):")
+            val islem: String = readln()
+
+            when (islem) {
+                "*" -> println(x * y)
+                "/" -> bolme(x, y)
+                "+" -> println(x + y)
+                "-" -> println(x - y)
+                "toplamının karakökü" -> println(Math.sqrt(x + y))
+                "mod" -> println(x % y)
+                else -> {
+                    println("Yanlış işlem girdiniz")
+                    yeniden_baslat()
                 }
-            }}
-
-
-
-
-
-    else-> {
-
-      yalnis_islem()
-
+            }
+            yeniden_baslat()
+        }
+        else -> {
+            println("Yanlış seçim yaptınız")
+            yeniden_baslat()
+        }
     }
-    }
-
 }
-
-fun decimalToBinary(decimal: Int): String{
-    var num = decimal
-    var binary = ""
-    if (num == 0) {
-        return "0"
-    }
-    while (num > 0) {
-        val remainder = num % 2
-        binary = remainder.toString() + binary
-        num /= 2
-    }
-    return binary
-}
-
--- src/toplama.kt
+++ src/toplama.kt
@@ -1,32 +1,71 @@
-fun yeniden_baslat(){
+import kotlin.math.pow
+
+fun yeniden_baslat() {
     println("Programı yeniden başlatmak istiyor musunuz? (e/h)")
-    val test: String = readLine() ?: ""
-    when(test){
+    val cevap: String = readln()
+    when (cevap.lowercase()) {
         "y", "e", "yes", "evet" -> programRestart()
-      else -> {}
+        else -> println("Program sonlandırıldı.")
     }
 }
 
 fun programRestart() {
     println("Kod 3 saniye içinde yeniden başlıyor...")
-    for (i in 3 downTo 0) {
+    for (i in 3 downTo 1) {
+        println("$i...")
         Thread.sleep(1000)
-        println(i)
     }
-    println("\n\n\n\n\n\n\n\n\n\n")
-    return main()
+    println("Başlatılıyor...\n\n\n\n\n\n\n\n\n\n")
+    main()
 }
 
-fun yalnis_islem(){
-    println("yalnis tusa basildi")
-    programRestart()
+fun decimalToBinary(decimal: Int): String {
+    var num = decimal
+    var binary = ""
+    if (num == 0) {
+        return "0"
+    }
+    while (num > 0) {
+        val remainder = num % 2
+        binary = remainder.toString() + binary
+        num /= 2
+    }
+    return binary
 }
-fun bolme(x:Double,y:Double){
-    if (y==0.00){
-        println("0 a bolemezsiniz")
-        programRestart()
+
+fun binaryToDecimal(binary: String): Int {
+    var decimal = 0
+    var power = binary.length - 1
+    for (digit in binary) {
+        if (digit == '1') {
+            decimal += 2.0.pow(power.toDouble()).toInt()
+        }
+        power -= 1
     }
-    else{
-        println(x/y)
+    return decimal
+}
+
+fun stringToBinary(text: String): String {
+    val binaryResult = StringBuilder()
+    for (char in text) {
+        val binaryChar = Integer.toBinaryString(char.code)
+        val paddedBinaryChar = binaryChar.padStart(8, '0')
+        binaryResult.append(paddedBinaryChar).append(" ")
     }
+    return binaryResult.toString().trim()
+}
+
+fun bolme(x: Double, y: Double) {
+    if (y == 0.0) {
+        println("0'a bölemezsiniz")
+        yeniden_baslat()
+    } else {
+        println(x / y)
+        yeniden_baslat()
+    }
+}
+
+fun yalnis_islem() {
+    println("Yanlış tuşa bastınız")
+    yeniden_baslat()
 }
